05a140ddd7183c2271a450d3269ff527ed1976ae
Andrés Torres <125112555+andres-stg@users.noreply.github.com>
2024-05-05 04:23:44 +0000

    created user, followers, post, comment, likes and media_file classes

--- src/models.py
+++ src/models.py
@@ -1,32 +1,61 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
-from eralchemy2 import render_er
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    profile_name = Column(String(50), nullable=False)user,
+    followers = relationship("Followers", foreign_keys="[Followers.followed_id]")
+    followed = relationship("Followers", foreign_keys="[Followers.follower_id]")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Followers(Base):
+    __tablename__ = 'followers'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    follower_id = Column(Integer, ForeignKey('user.id'))
+    followed_id = Column(Integer, ForeignKey('user.id'))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    created_datetime = Column(DateTime(timezone=True), server_default=func.now())
+    created_by_user_id = Column(Integer, ForeignKey('user.id'))
+    location = Column(String(250), nullable=False)
+    caption = Column(String(250), nullable=True)
+    user = relationship("User")
+    likes = relationship("Likes")
+    comments = relationship("Comment")
+    media_type = relationship("MediaType")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    created_datetime = Column(DateTime(timezone=True), server_default=func.now())
+    posted_by_user = Column(Integer, ForeignKey('user.id'))
+    comment_on_post = Column(String(350), nullable=False)
+
+class Likes(Base):
+    __tablename__ = 'likes'
+    id_user = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'), primary_key=True)
+
+class MediaType(Base):
+    __tablename__ = 'media_type'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    media_file = Column(String(250), nullable=False)
+    post = relationship("Post")
 
     def to_dict(self):
         return {}
+
 
 ## Draw from SQLAlchemy base
 try:
